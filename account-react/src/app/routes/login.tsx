8dba603d1f2fb896df47fbec9b0aff25e8ff39f6
daigo uchida <daigo.densiaddress@gmail.com>
2025-04-23 13:46:56 +0900

    update: ある程度のstyle完成

--- account-react/src/app/routes/login.tsx
+++ account-react/src/app/routes/login.tsx
@@ -2,6 +2,10 @@ import React , { useState } from 'react';
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import Header from '../../components/ui/header';
+import { GoArrowLeft } from "react-icons/go";
+import { Link } from 'react-router-dom';
+import { paths } from '../../config/paths';
+
 
 const Login = () =>{
     const [user_id, setUser_Id] = useState('');
@@ -27,22 +31,30 @@ const Login = () =>{
     };
     return(
         <>
-        <Header/>
-        <div className='main'>
-            <h1>ログイン</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="user_id">ユーザーID:</label>
-                    <input type="text" id="user_id" value={user_id} onChange={(e) => setUser_Id(e.target.value)} />
+                <Header/>
+                <div className='main'>
+                    <div className='backbox'>
+
+                        <h1>ログイン</h1>
+                        <Link to={paths.Welcome.getHref() } className='back-link' >
+                            <GoArrowLeft size={30} title='戻る' />
+                        </Link>
+                            
+
+                        <form onSubmit={handleSubmit} className='signupform'>
+                            <div className='inputtext'>
+
+                                <label htmlFor="user_id">ユーザーID:</label>
+                                <input type="text" id="user_id" value={user_id} onChange={(e) => setUser_Id(e.target.value)} />
+                            
+                                <label htmlFor="password">パスワード:</label>
+                                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                            </div>
+
+                            <button type="submit" className='submitButton'>ログイン</button>
+                        </form>
+                    </div>
                 </div>
-                <div>
-                    <label htmlFor="password">パスワード:</label>
-                    <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                {error && <p style={{ color: 'red' }}>{error}</p>}
-                <button type="submit">ログイン</button>
-            </form>
-        </div>
         </>
     );
 };
--- account-react/src/app/routes/signup.tsx
+++ account-react/src/app/routes/signup.tsx
@@ -5,6 +5,9 @@ import Header from '../../components/ui/header';
 import { GoArrowLeft } from "react-icons/go";
 import { paths } from '../../config/paths';
 import { Link } from 'react-router-dom';
+
+import '../../components/style/singup.css';
+
 const Signup = () =>{
         const [user_id, setUser_Id] = useState('')
         const [password, setPassword] = useState('')
@@ -40,30 +43,35 @@ const Signup = () =>{
                 <>
                 <Header/>
                 <div className='main'>
-                <Link to={paths.Welcome.getHref()}>
-                    <GoArrowLeft/> 
-                </Link>
-                    <h1>アカウント新規登録</h1>
-                    
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <label htmlFor="user_id">ユーザーID:</label>
-                            <input type="text" id="user_id" value={user_id} onChange={(e) => setUser_Id(e.target.value)} />
-                        </div>
-                        <div>
-                            <label htmlFor="nickname">ニックネーム:</label>
-                            <input type="text" id="nickname" value={nickname} onChange={(e) => setNickname(e.target.value)} />
-                        </div>
-                        <div>
-                            <label htmlFor="password">パスワード:</label>
-                            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        </div>
-                        <div>
-                            <label htmlFor="password_confirm">パスワード確認:</label>
-                            <input type="password" id="password_confirm" value={password_confirmation} onChange={(e) => setPassword_Confirm(e.target.value)} />
-                        </div>
-                        <button type="submit">登録</button>
-                    </form>
+                    <div className='backbox'>
+
+                        <h1>アカウント新規登録</h1>
+                        <Link to={paths.Welcome.getHref() } className='back-link' >
+                            <GoArrowLeft size={30} title='戻る' />
+                        </Link>
+                            
+
+                        <form onSubmit={handleSubmit} className='signupform'>
+                            <div className='inputtext'>
+                                <label htmlFor="user_id">ユーザーID:</label>
+                                <input type="text" id="user_id" value={user_id} onChange={(e) => setUser_Id(e.target.value)} />
+                            
+                            
+                                <label htmlFor="nickname">ニックネーム:</label>
+                                <input type="text" id="nickname" value={nickname} onChange={(e) => setNickname(e.target.value)} />
+                            
+                            
+                                <label htmlFor="password">パスワード:</label>
+                                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                            
+                            
+                                <label htmlFor="password_confirm">パスワード確認:</label>
+                                <input type="password" id="password_confirm" value={password_confirmation} onChange={(e) => setPassword_Confirm(e.target.value)} />
+                            </div>
+
+                            <button type="submit" className='submitButton'>登録</button>
+                        </form>
+                    </div>
                 </div>
                 </>
             );
