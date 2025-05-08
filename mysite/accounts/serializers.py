6305f571ba6cc0a4ff0ea4abe964834790b879b4
daigo-uchida <daigo.densiaddress@gmail.com>
2025-05-08 15:58:04 +0900

    perf: viewの変更に伴いRegisterSerializerにパスワード確認を追加、またauthticateを追加しDBアクセス回数を削減

--- mysite/accounts/serializers.py
+++ mysite/accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import User
-
+from django.contrib.auth import authenticate
 #ユーザー登録シリアライザ
 class  RegisterSerializer(serializers.ModelSerializer):
     #userメタ情報
@@ -9,33 +9,32 @@ class  RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ('user_id', 'password', 'nickname')
         extra_kwargs = {'password': {'write_only': True}} #パスワード非表示
-
+        
+    def validate(self, data):
+        if data['password'] != data['password_confirmation']:
+            raise serializers.ValidationError('パスワードが一致しません')
+        return data
+    
         #ユーザー作成関数
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            return user
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
         
 #ユーザーログインシリアライザ
 class LoginSerializer(serializers.Serializer):
     #入力
-    user_id = serializers.CharField(max_length=255,write_only=True)
-    password = serializers.CharField(write_only=True,style={'input_type':'password'})
 
     def validate(self,data):
         #DB内検索
         user_id = data.get('user_id')
         password = data.get('password')
-        userid = User.objects.get(user_id = user_id)
-        re_password = User.objects.get(password = password)
+        user = authenticate(user_id = user_id, password = password)
+        #ユーザーが存在するか確認
+        if user is not None:
+            return data
+        else:
+            raise serializers.ValidationError('ログイン失敗')
 
-        #検証
-        if user_id == userid.user_id:
-            if password == re_password.password:
-                return data
-            
-            else:
-                raise serializers.ValidationError('ログイン失敗')
-            
 
 #ユーザー情報更新シリアライザ
 class UserUpdateSerializer(serializers.Serializer):
@@ -46,5 +45,5 @@ class UserUpdateSerializer(serializers.Serializer):
         instance.nickname = validated_data.get('nickname',instance.nickname)
         instance.comment = validated_data.get('comment',instance.comment)
         instance.save()
-        return instance
-    
+        return instance  
+
