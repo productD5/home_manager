209103530e8107a156d7a7c2017735dbaeae9fda
daigo uchida <daigo.densiaddress@gmail.com>
2025-05-08 14:43:20 +0900

    update:home_moneyモデルを作成

--- mysite/home_manager/models.py
+++ mysite/home_manager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class home_money(models.Model):
+    user_id = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    money_id = models.IntegerField(unique=True, primary_key=True, autoincrement=True)
+    money = models.IntegerField(default=0)
+    category = models.CharField(max_length=20)
+    title = models.CharField(max_length=20)
+    money_comment = models.CharField(max_length=100, blank=True)
