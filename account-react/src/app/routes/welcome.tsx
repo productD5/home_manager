25f0d55f481db783fb0ebc8da0c91bb52ee49c86
daigo-uchida <daigo.densiaddress@gmail.com>
2025-05-22 10:33:10 +0900

    fix :@を使った動的パス指定に

--- account-react/src/app/routes/welcome.tsx
+++ account-react/src/app/routes/welcome.tsx
@@ -1,26 +1,25 @@
 import React from "react";
-import Header from "../../components/ui/header";
-import "../../components/style/welcome.css"
+import Header from "@/components/ui/header";
+import "@/components/style/welcome.css";
 import { Link } from "react-router-dom";
-import {paths} from "../../config/paths"
+import { paths } from "@/config/paths";
 
-const Welcome = () =>{
-    return(
+const Welcome = () => {
+  return (
     <>
-    <Header/>
-    <div className="main">
-        
+      <Header />
+      <div className="main">
         <div className="back-box">
-                <Link to={paths.Login.getHref()}>
-                    <button className="button">ログイン</button>
-                </Link>
-                <Link to={paths.sign_up.getHref()}>
-                    <button className="button">新規登録</button>
-                </Link>
+          <Link to={paths.Login.getHref()}>
+            <button className="button">ログイン</button>
+          </Link>
+          <Link to={paths.sign_up.getHref()}>
+            <button className="button">新規登録</button>
+          </Link>
         </div>
-    </div>
+      </div>
     </>
-    );
+  );
 };
 
-export default Welcome
+export default Welcome;
