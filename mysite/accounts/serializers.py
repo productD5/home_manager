d6be58312f763e9e8e71307349291a83bde687e2
daigo-uchida <daigo.densiaddress@gmail.com>
2025-05-22 10:42:19 +0900

    fix : ユーザーモデル変更に伴う修正

--- mysite/accounts/serializers.py
+++ mysite/accounts/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import User
-from django.contrib.auth import authenticate
 #ユーザー登録シリアライザ
 class  RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=30, write_only=True)
+    password_confirmation = serializers.CharField(max_length=30, write_only=True)
     #userメタ情報
     class Meta:
         #シリアライザに渡すモデルを指定
         model = User
-        fields = ('user_id', 'password', 'nickname')
+        fields = ('user_id','email','nickname','password','password_confirmation')
         extra_kwargs = {'password': {'write_only': True}} #パスワード非表示
         
     def validate(self, data):
@@ -17,29 +18,37 @@ class  RegisterSerializer(serializers.ModelSerializer):
     
         #ユーザー作成関数
     def create(self, validated_data):
+        validated_data.pop('password_confirmation')
         user = User.objects.create_user(**validated_data)
         return user
         
 #ユーザーログインシリアライザ
 class LoginSerializer(serializers.Serializer):
     #入力
+    user_id = serializers.CharField(max_length=30)
+    password = serializers.CharField(max_length=30, write_only=True)
 
     def validate(self,data):
         #DB内検索
         user_id = data.get('user_id')
         password = data.get('password')
-        user = authenticate(user_id = user_id, password = password)
-        #ユーザーが存在するか確認
-        if user is not None:
-            return data
-        else:
-            raise serializers.ValidationError('ログイン失敗')
+        try:
+            user = User.objects.get(user_id=user_id)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("user_idかパスワードが正しくありません!")
+
+        if not user.check_password(password):
+            #パスワードが一致しない場合
+            raise serializers.ValidationError("user_idかパスワードが正しくありません")
+
+        return data
 
 
 #ユーザー情報更新シリアライザ
 class UserUpdateSerializer(serializers.Serializer):
     nickname = serializers.CharField(max_length=30, allow_blank=True)
     comment = serializers.CharField(max_length=100, allow_blank=True)
+    
 
     def update(self, instance, validated_data):
         instance.nickname = validated_data.get('nickname',instance.nickname)
