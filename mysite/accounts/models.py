caca5f7b3d331122b7d5cda5db0164164126b061
daigo-uchida <daigo.densiaddress@gmail.com>
2025-05-22 10:40:32 +0900

    fix : userモデルをカスタムユーザーモデルに変更

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -1,50 +1,33 @@
 from django.db import models
-import hashlib
-from datetime import timedelta
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
 from django.utils import timezone
-
-class User(models.Model):
-    user_id = models.CharField(max_length=20, unique=True) #ユニークな値
-    password = models.CharField(max_length=20)
-    nickname = models.CharField(max_length=50)
-    comment = models.CharField(max_length=100, blank=True ) #空白ok
-
-
-def in_30_days():
-    return timezone.now() + timedelta(days=30) #現在の時間から30日足したものを返す
-
-class AccessToken(models.Model):
-    #紐づけるユーザー
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-
-    #アクセストークン(最大40文字)
-    token = models.CharField(max_length=40)
-
-    #アクセス日時
-    access_datetime = models.DateTimeField(default=in_30_days)
-
-    def str(self):
-        #メールアドレスとアクセス日時、トークンが見えるように設定
-        dt = timezone.localtime(self.access_datetime).strftime('%Y/%m/%d %H:%M:%S')
-        return self.user.user_id + '(' + dt + ') - ' + self.token
+class UserManager(BaseUserManager):
+    def create_user(self, user_id, password=None, **extra_fields):
+        if not user_id:
+            raise ValueError('ユーザーIDは必須です')
+        user = self.model(user_id=user_id, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
     
-    @staticmethod
-    def create(user: User):
-        #既存のトークンを取得
-        if AccessToken.objects.filter(user=user).exists(): #レコードがある場合はTrue
-            #トークンが存在していた場合削除
-            AccessToken.objects.get(user=user).delete()
+    def create_superuser(self, user_id, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(user_id, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    user_id = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    nickname = models.CharField(max_length=30, blank=True)
+    comment = models.CharField(max_length=100, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-        #トークンの作成(user_id、pasword、システムの日付のハッシュ値とする)
-        dt = timezone.now()
-        str = user.user_id + user.password + dt.strftime('%Y%m%d%H%M%S%f')
-        #SHA1でハッシュ化
-        hash = hashlib.sha1(str.encode('utf-8')).hexdigest()
+    objects = UserManager()
 
-        #DBに追加情報を記録
-        token = AccessToken.objects.create(
-            user=user,
-            token=hash,
-            access_datetime=dt
-        )
-        return token
+    USERNAME_FIELD = 'user_id'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.user_id
+
