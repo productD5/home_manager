69cd2e35e8cc766678c810257b2c5083b963d26f
daigo-uchida <daigo.densiaddress@gmail.com>
2025-05-22 10:48:43 +0900

    refactor: シリアライザークラス名変更に伴う修正

--- mysite/home_manager/paths.py
+++ mysite/home_manager/paths.py
@@ -2,5 +2,5 @@ from . import views;
 from django.urls import path;
 
 urlpatterns = [
-    path('register/', views.HomeRegisterView.as_view(), name='home-register'), #家計情報登録
+    path('register/', views.addMoneyView.as_view(), name='home-register'), #家計情報登録
 ]
--- mysite/home_manager/views.py
+++ mysite/home_manager/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import HomeMoneyRegistrSerializer
+from .serializers import addMoneySerializer
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
-class HomeRegisterView(APIView):
+class addMoneyView(APIView):
     """新規登録"""
     permission_classes = [IsAuthenticated] #認証が必要なAPIです
 
     def post(request, *args, **kwargs):
-        serializer = HomeMoneyRegistrSerializer(data=request.data)
+        serializer = addMoneySerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             # エラーなし
             try:
