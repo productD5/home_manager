0413ce5d518791fc709d266f7b1d186a04a6bf8a
daigo-uchida <daigo.densiaddress@gmail.com>
2025-05-27 11:25:17 +0900

    refactor:セミコロンの追加など

--- account-react/src/app/routes/not-found.tsx
+++ account-react/src/app/routes/not-found.tsx
@@ -1,13 +1,11 @@
- const Not_found = () =>{
-    return(
-        <>
-        <h1> 404 </h1>
-        <h1> File Not Found </h1>
-        <h1>アクセスしようとしたページは見つかりませんでした。</h1>
-
-        </>
-
-    );
+const Not_found = () => {
+  return (
+    <>
+      <h1> 404 </h1>
+      <h1> File Not Found </h1>
+      <h1>アクセスしようとしたページは見つかりませんでした。</h1>
+    </>
+  );
 };
 
-export default Not_found
+export default Not_found;
