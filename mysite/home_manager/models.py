73fb5afc74f4740e827b7f8945caf453817b3799
daigo-uchida <daigo.densiaddress@gmail.com>
2025-06-11 10:03:17 +0900

    fix:idカラムにエラーが生じたため外部キーをidからuser_idに変更

--- mysite/home_manager/models.py
+++ mysite/home_manager/models.py
@@ -3,7 +3,7 @@ from accounts.models import User
 # Create your models here.
 
 class home_money(models.Model):
-    id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     money_id = models.AutoField(unique=True, primary_key=True,)
     money = models.IntegerField(default=0)
     category = models.CharField(max_length=20)
