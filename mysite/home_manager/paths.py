2874682edd2eb51de4c69b0e53607b9cfea7991a
daigo-uchida <daigo.densiaddress@gmail.com>
2025-07-02 10:32:18 +0900

    feat : 出費情報の追加、編集、削除機能実装に伴うAPIパスの追加

--- mysite/home_manager/paths.py
+++ mysite/home_manager/paths.py
@@ -3,5 +3,7 @@ from django.urls import path;
 
 urlpatterns = [
     path('register/', views.addMoneyView.as_view(), name='home-register'), #家計情報登録
-    
+    path('view/<str:user_id>', views.moneyDataView.as_view(), name='home-view'), #家計情報取得
+    path('editMoney/<int:money_id>', views.updateMoneyView.as_view(), name='home-update'), #家計情報更新
+    path('deleteMoney/<int:money_id>', views.deleteMoneyView.as_view(), name='home-delete'), #家計情報削除
 ]
