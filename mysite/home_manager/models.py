bcbaa258d46a7e835130c494eee8ce4fa3790187
daigo-uchida <daigo.densiaddress@gmail.com>
2025-07-02 10:31:23 +0900

    fix : 支出データの紐づかせ方の問題で取得がうまくいってなかったため、修正

--- mysite/home_manager/models.py
+++ mysite/home_manager/models.py
@@ -3,7 +3,7 @@ from accounts.models import User
 # Create your models here.
 
 class home_money(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User,to_field="user_id" ,on_delete=models.CASCADE)
     money_id = models.AutoField(unique=True, primary_key=True,)
     money = models.IntegerField(default=0)
     category = models.CharField(max_length=20)
